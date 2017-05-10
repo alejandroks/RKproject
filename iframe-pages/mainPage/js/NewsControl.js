b7b0ff945f34f013a31c746ffa7d92c64b8d0a54
Alex Home <alejandroks02@gmail.com>
2017-05-10 08:13:04 +0300

    Delete table add div News menu

--- iframe-pages/mainPage/js/NewsControl.js
+++ iframe-pages/mainPage/js/NewsControl.js
@@ -1,47 +1,40 @@
 /**
  * Created by Administrator on 05.05.2017.
  */
-function NewsControl(options){
-var selfref= this;
+function NewsControl(options) {
+    var selfref = this;
     selfref.counteiner = options.counteiner;
     selfref.NewsCreate();
 }
-NewsControl.prototype={
-    constructor:NewsControl,
-    NewsCreate:function(){
-        var counteiner= this.counteiner;
+NewsControl.prototype = {
+    constructor: NewsControl,
+    NewsCreate: function () {
+        var counteiner = this.counteiner;
         var divBoxNews = document.createElement('div');
         divBoxNews.className = "NewsBox";
-        var table=this.TableCreate();
-        divBoxNews.appendChild(table);
+        var ElementNews = this.NewsElementCreate();
+        divBoxNews.appendChild(ElementNews);
         counteiner.appendChild(divBoxNews);
 
     },
-    TableCreate: function(){
-        var table = document.createElement('table');
-        table.cellPadding=20;
-        table.cellSpacing=20;
-        var data= this.getData();
-        var content;
-        for(var i=0;i<data.length;i++){
-            var tr = document.createElement('tr');
-            for(content in data[i]){
-                var td =document.createElement('td');
-                    td.className="ForImgTable";
-                tr.appendChild(td);
-                var m = content;
-                td.innerHTML=content;
-            }
-            table.appendChild(tr);
+    NewsElementCreate: function () {
+        var data = this.getData();
+        var newsElement = document.createElement('div');
+        newsElement.className = "news-element";
+        if (data != null && data != undefined) {
+            return newsElement;
         }
-        return table;
+        newsElement.className="exeptionNews";
+        newsElement.innerHTML="Новых Новостей нет, иди нахуй"
+        return newsElement;
     },
-    getData:function(){
-        var content={name:"Alex",url:"IMAGE"};
-        var data=[content,content,content];
-      return data;
+    getData: function () {
+        // var content = { name: "Alex", url: "IMAGE" };
+        // var data = [content, content, content];
+        var data;
+        return data;
     },
-    GetElement:function(id){
-       return document.getElementById(id);
+    GetElement: function (id) {
+        return document.getElementById(id);
     }
 }
