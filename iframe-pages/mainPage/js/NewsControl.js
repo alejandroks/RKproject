f3589ccc5750b917a7663b9bfc172216d1614a51
Alex Home <alejandroks02@gmail.com>
2017-05-11 03:11:10 +0300

    news..

--- iframe-pages/mainPage/js/NewsControl.js
+++ iframe-pages/mainPage/js/NewsControl.js
@@ -12,32 +12,46 @@ NewsControl.prototype = {
         var counteiner = this.counteiner;
         var newsBlock = this.NewsElementCreate();
         counteiner.appendChild(newsBlock);
-
     },
     NewsElementCreate: function () {
         var data = this.getData();
         var newsBlock = document.createElement('div');
         newsBlock.className = "NewsBlock";
-        if (data != null && data != undefined) {
-            for(var i = 0; i<data.length;i++){
-                var newsRow=document.createElement('div');
-                newsRow.className="NewsRow";
-                for(var dataIndex in data[i]){
-                    var elemNews=document.createElement("div");
-                    elemNews.className="newsCells";
-                    elemNews.innerHTML=data[i][dataIndex];
-                    newsRow.appendChild(elemNews);
+        if (data) {
+            for (var i = 0; i < data.length; i++) {
+                var newsRow = document.createElement('div');
+                newsRow.className = "NewsRow";
+
+                var dataItem = data[i];
+                var elemNews = document.createElement("div");
+                elemNews.className = "newsCell";
+
+                elemNews.onclick = function () {
+                    this.classList.toggle("expanded");
                 }
+
+                elemNews.appendChild(document.createTextNode(dataItem.name));
+                newsRow.appendChild(elemNews);
+
+                var elemNews = document.createElement("div");
+                elemNews.className = "newsCell";
+
+                var newsImg = elemNews.appendChild(document.createElement('img'));
+                newsImg.className = "news-url";
+                newsImg.setAttribute("url", dataItem.url);
+
+                newsRow.appendChild(elemNews);
                 newsBlock.appendChild(newsRow);
             }
+
             return newsBlock;
         }
-        newsBlock.className="exeptionNews";
-        newsBlock.innerHTML="Новых Новостей нет, иди нахуй";
+        newsBlock.className = "exeptionNews";
+        newsBlock.innerHTML = "Новых Новостей нет, иди нахуй";
         return newsBlock;
     },
     getData: function () {
-        var content = { name: "Alex", url: "IMAGE" };
+        var content = { name: "AlexAlexAlexAlexAlexAlexAlexAlexAlex Alex Alex Alex Alex Alex Alex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex AlexAlex Alex Alex Alex Alex ", url: "IMAGE" };
         var data = [content, content, content];
         return data;
     }
