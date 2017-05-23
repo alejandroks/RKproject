e1a7f3972f86cdd73c71d34e4f0129e44c97b65f
Alex <alejandroks02@gmail.com>
2017-05-23 15:39:36 +0200

    add MainControl

---
+++ script/MainIndexControl.js
@@ -0,0 +1,39 @@
+
+function MainIndexControl(options){
+    var selfref = this;
+    var counteiner=options.counteiner;
+    selferf.MainIndexControlCreateMenuTopControl();
+    selferf.IframeSizeSet();
+}
+MainIndexControl.prototype={
+    constructor: MainIndexControl,
+    MainIndexControlCreateMenuTopControl:function(){
+        var leftButtonMenu = $('#buttonMenuLeft');
+        leftButtonMenu.click(function OnClickButton(){
+
+            selfref.MenuShow(leftButtonMenu);
+    })
+    },
+    MenuShow: function(menuControlObj){
+
+    }
+    ,
+    MainControlAnimationMenu: function(){
+
+    },
+    MainControlAnimationStartPlay: function(){
+
+    },
+    IframeSizeSet: function(){
+        var iframe= $('#mainIframe').contents();
+        var newsBlockParentIframe = $('#innerCenter');
+        var iframeSize=iframe.find('body').height();
+        newsBlockParentIframe.height(iframeSize);
+        if( iframe.find('.newsCellText')){
+            iframe.find('.newsCellText').click(function(){
+                var iframeSize=iframe.find('body').height();
+                newsBlockParentIframe.height(iframeSize);
+            })
+        }
+    }
+};
