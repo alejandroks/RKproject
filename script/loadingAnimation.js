91630e55d2213387cd13efa572c407ee33dd78e8
Alex Home <alejandroks02@gmail.com>
2017-05-23 08:49:34 +0300

    news

--- script/loadingAnimation.js
+++ script/loadingAnimation.js
@@ -1,25 +1,36 @@
 
-window.onload=function(){
-    var firstLine =document.getElementById("expand");
-    var secondLine =document.getElementById("expand2");
+window.onload = function () {
+    var firstLine = document.getElementById("expand");
+    var secondLine = document.getElementById("expand2");
     var buttonLeft = document.getElementById("buttonMenuLeft");
-    var buttonRigth= document.getElementById("buttonRightMenu");
-    var LoadingLineBot=document.getElementById("LoadingLineBot");
+    var buttonRigth = document.getElementById("buttonRightMenu");
+    var LoadingLineBot = document.getElementById("LoadingLineBot");
 
-    buttonLeft.onclick=function ButtonClick(){
+    buttonLeft.onclick = function ButtonClick() {
         firstLine.style.display = "block";
         secondLine.style.display = "block";
     };
-    buttonRigth.onclick=function ButtonClick(){
+    buttonRigth.onclick = function ButtonClick() {
         firstLine.style.display = "block";
         secondLine.style.display = "block";
-        secondLine.id="expand2_otherAnimation";
-        secondLine.style.webkitAnimationPlayState="running";
+        secondLine.id = "expand2_otherAnimation";
+        secondLine.style.webkitAnimationPlayState = "running";
 
     };
-    var sizeElement=document.getElementsByTagName('iframe')[0];
-    var s=sizeElement.contentWindow.document.body.scrollHeight;
-    var center=document.getElementById('innerCenter');
-   //alert(getComputedStyle(sizeElement).height);
-    center.style.height=s;
+    var sizeElement = document.getElementById('ourframe');
+  //  var s = sizeElement.contentWindow.document.body.scrollHeight;
+    var center = document.getElementById('innerCenter');
+    //alert(getComputedStyle(sizeElement).height);
+  //  center.style.height = 800;
+
+
+    // $(sizeElement).load(function () {
+
+        var iframe = $(sizeElement).contents();
+
+        iframe.find(".newsCellText").click(function () {
+            var iframeBody = iframe.find('body');
+            $(center).height(iframeBody.height());
+        });
+    // });
 };
